9de8e5808954076df74d7aa2e0f311e707bea58b
Phanisment <agilseptiyono94@gmail.com>
2025-07-05 11:47:43 +0700

    Add gitnore

--- a.sh
+++ a.sh
@@ -1,6 +1,21 @@
-git init
+#!/bin/bash
+#cd /storage/emulated/0/ACode/Projects/Coding/Fabric/library
+
+if [ ! -d ".git" ]; then
+	echo "Error: This is not git repository."
+	exit 1
+fi
+
+read -p "Enter the message commit: " commit_message
 git add .
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/Phanisment/Enlib.git
-git push -u origin master
+git commit -m "$commit_message"
+echo "Successfuly commit with message: $commit_message"
+read -p "Enter the branch name for push (default: master): " branch_name
+branch_name=${branch_name:-master}
+git push origin "$branch_name"
+
+if [ $? -eq 0 ]; then
+	echo "Successfuly push to branch: $branch_name"
+else
+	echo "Error: Push failed."
+fi
